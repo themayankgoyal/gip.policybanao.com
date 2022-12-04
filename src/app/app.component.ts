1a56da3abb0c098edbc379d5032eebe53458e120
themayankgoyal <mayankgoyal1032000@gmail.com>
2022-12-04 11:53:37 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+  interface City {
+  name: string,
+  code: string
+}
 export class AppComponent {
   title = 'my-app';
+  cities: City[];
+  selectedCity!: City;
+
+  constructor() {
+    this.cities = [
+      { name: 'New York', code: 'NY' },
+      { name: 'Rome', code: 'RM' },
+      { name: 'London', code: 'LDN' },
+      { name: 'Istanbul', code: 'IST' },
+      { name: 'Paris', code: 'PRS' }
+    ];
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DropdownModule} from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { DropdownFilterOptions } from 'primeng/dropdown';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    DropdownModule,
+    FormsModule,
+    DropdownFilterOptions
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
